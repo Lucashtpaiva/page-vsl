986b7cf6945b46ab7f0579c15e15228168496507
Lucas Paiva <lucashtpaiva@gmail.com>
2025-02-19 20:11:55 -0300

    initial configs

---
+++ eslint.config.mjs
@@ -0,0 +1,48 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: [
+      "@typescript-eslint",
+      "react",
+      "prettier",
+      "eslint-plugin-import-helpers",
+    ],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "prettier/prettier": "error",
+      "import-helpers/order-imports": [
+        "warn",
+        {
+          newlinesBetween: "always",
+          groups: [
+            ["/^react/", "/^next/", "/@next/"],
+            "module",
+            ["absolute", "/^components/"],
+            "/^@shared/",
+            ["parent", "sibling", "index"],
+          ],
+          alphabetize: { order: "asc", ignoreCase: true },
+        },
+      ],
+    },
+  }),
+];
+
+export default eslintConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <div>Teste</div>
+    </main>
+  );
+}
