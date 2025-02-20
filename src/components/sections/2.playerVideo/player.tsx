e23f0dc63b6f9bfa4984f49e03abeb238758c498
Lucas Paiva <78661939+Lucashtpaiva@users.noreply.github.com>
2025-02-20 04:38:31 -0300

    finalizando a seção de player de vídeo (#2)

--- src/components/sections/2.playerVideo/player.tsx
+++ src/components/sections/2.playerVideo/player.tsx
@@ -5,13 +5,19 @@ import { ArrowRight } from 'lucide-react';
 
 export default function Player() {
   return (
-    <Container className="justify-center bg-[url(/bg-desktop.webp)]">
-      <SectionBox>
-        <h3 className="text-center text-3xl font-extrabold uppercase">Logo</h3>
-        <h1 className="max-w-[60rem] text-center text-2xl font-extrabold uppercase antialiased md:text-4xl md:leading-12">
-          Impulsione o crescimento da sua empresa com inovação, automação e
-          tecnologia digital de última geração!
+    <Container className="justify-center bg-[url(/video-desktop.webp)]">
+      <SectionBox className="py-20 md:py-32">
+        <h1 className="max-w-[60rem] text-center text-2xl font-extrabold uppercase antialiased md:text-3xl md:leading-12">
+          Inovação para seu negócio!
         </h1>
+        <div className="aspect-video w-full max-w-[860px] md:h-[500px]">
+          <iframe
+            src="https://www.youtube.com/embed/c5rWB_fS5ao?si=w-InpASwuXmv3GxT"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            className="h-full w-full rounded-lg"
+          ></iframe>
+        </div>
+
         <Button>
           Fale conosco
           <ArrowRight />
