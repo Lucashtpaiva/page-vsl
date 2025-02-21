17d1010b26d738e392ba2cd5a9dd0e7d92299975
Lucas Paiva <lucashtpaiva@gmail.com>
2025-02-21 10:14:21 -0300

    Carregamento da página antes da interação com usuário

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -7,6 +7,7 @@ export function Button({ href, className, children, ...props }: ButtonProps) {
   return (
     <Link
       href={href}
+      prefetch={true}
       className={`bg-white-700 text-black-600 hover:bg-white-500 flex cursor-pointer items-center justify-center gap-2 rounded-lg px-5 py-2 text-center font-bold uppercase transition-colors duration-300 ${className}`}
       {...props}
     >
